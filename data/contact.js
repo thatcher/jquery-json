ebd6b08968d3a1d25c73007c37df69fd9331b4d0
thatcher <thatcher.christopher@gmail.com>
2008-12-08 10:35:17 -0500

    Changed name to reflect that the plugin is more generally json + jspath.  cleaned up plugin interface to jquery

---
+++ data/contact.js
@@ -0,0 +1,13 @@
+{
+    "$firstName": "Jason",
+    "$lastName": "Jones",
+    "address": {
+        "$city": "San Francisco, CA",
+        "$zip": 94121,
+        "$address": "444 Columbus Ave"
+    },
+    "email": [
+        "jason@sf.com",
+        "sjones@adobe.com"
+       ]
+}
---
+++ data/flicker.js
@@ -0,0 +1,229 @@
+{
+		"title": "Recent Uploads tagged cat",
+		"link": "http://www.flickr.com/photos/tags/cat/",
+		"description": "",
+		"modified": "2008-10-12T04:13:06Z",
+		"generator": "http://www.flickr.com/",
+		"items": [
+	   {
+			"title": "IMGP3719",
+			"link": "http://www.flickr.com/photos/takewrning/2933649792/",
+			"media": {"m":"http://farm4.static.flickr.com/3198/2933649792_5bf8492e5a_m.jpg"},
+			"date_taken": "2008-10-12T00:02:26-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/takewrning/&quot;&gt;Petie_in_Cbus&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/takewrning/2933649792/&quot; title=&quot;IMGP3719&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3198/2933649792_5bf8492e5a_m.jpg&quot; width=&quot;160&quot; height=&quot;240&quot; alt=&quot;IMGP3719&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:13:06Z",
+			"author": "nobody@flickr.com (Petie_in_Cbus)",
+			"author_id": "56257767@N00",
+			"tags": "cats cat creatures"
+	   },
+	   {
+			"title": "ASHA AND FRIENDS ... FLUFF n' HARD EDGES n' CURVES",
+			"link": "http://www.flickr.com/photos/22522979@N02/2933654410/",
+			"media": {"m":"http://farm4.static.flickr.com/3240/2933654410_a0c68381e0_m.jpg"},
+			"date_taken": "2008-10-11T21:15:11-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/22522979@N02/&quot;&gt;great2c&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/22522979@N02/2933654410/&quot; title=&quot;ASHA AND FRIENDS ... FLUFF n' HARD EDGES n' CURVES&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3240/2933654410_a0c68381e0_m.jpg&quot; width=&quot;240&quot; height=&quot;166&quot; alt=&quot;ASHA AND FRIENDS ... FLUFF n' HARD EDGES n' CURVES&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;asha and friends for katie&amp;amp;brian and Frenchies!&lt;br /&gt; HELLO FROM HOLLYWOOD!&lt;/p&gt;",
+			"published": "2008-10-12T04:15:11Z",
+			"author": "nobody@flickr.com (great2c)",
+			"author_id": "22522979@N02",
+			"tags": "cats cat kitten"
+	   },
+	   {
+			"title": "IMGP3721",
+			"link": "http://www.flickr.com/photos/takewrning/2933651148/",
+			"media": {"m":"http://farm4.static.flickr.com/3251/2933651148_4a7f4d171d_m.jpg"},
+			"date_taken": "2008-10-12T00:02:58-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/takewrning/&quot;&gt;Petie_in_Cbus&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/takewrning/2933651148/&quot; title=&quot;IMGP3721&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3251/2933651148_4a7f4d171d_m.jpg&quot; width=&quot;240&quot; height=&quot;160&quot; alt=&quot;IMGP3721&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:13:44Z",
+			"author": "nobody@flickr.com (Petie_in_Cbus)",
+			"author_id": "56257767@N00",
+			"tags": "cats cat creatures"
+	   },
+	   {
+			"title": "",
+			"link": "http://www.flickr.com/photos/ameadowlark/2932791557/",
+			"media": {"m":"http://farm4.static.flickr.com/3146/2932791557_50d74d4bab_m.jpg"},
+			"date_taken": "2008-10-11T21:44:57-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/ameadowlark/&quot;&gt;a.meadowlark&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/ameadowlark/2932791557/&quot; title=&quot;&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3146/2932791557_50d74d4bab_m.jpg&quot; width=&quot;240&quot; height=&quot;161&quot; alt=&quot;&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:11:55Z",
+			"author": "nobody@flickr.com (a.meadowlark)",
+			"author_id": "82865775@N00",
+			"tags": "yellow cat table"
+	   },
+	   {
+			"title": "",
+			"link": "http://www.flickr.com/photos/coisasminhas/2933646284/",
+			"media": {"m":"http://farm4.static.flickr.com/3054/2933646284_eb9b0cd306_m.jpg"},
+			"date_taken": "2008-10-12T02:11:28-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/coisasminhas/&quot;&gt;Lucila.&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/coisasminhas/2933646284/&quot; title=&quot;&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3054/2933646284_eb9b0cd306_m.jpg&quot; width=&quot;240&quot; height=&quot;180&quot; alt=&quot;&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:11:28Z",
+			"author": "nobody@flickr.com (Lucila.)",
+			"author_id": "15191167@N02",
+			"tags": "cat gato criança menina"
+	   },
+	   {
+			"title": "Juan Travolta.",
+			"link": "http://www.flickr.com/photos/ameadowlark/2933648132/",
+			"media": {"m":"http://farm4.static.flickr.com/3283/2933648132_a5d468204b_m.jpg"},
+			"date_taken": "2008-10-11T21:47:38-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/ameadowlark/&quot;&gt;a.meadowlark&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/ameadowlark/2933648132/&quot; title=&quot;Juan Travolta.&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3283/2933648132_a5d468204b_m.jpg&quot; width=&quot;240&quot; height=&quot;161&quot; alt=&quot;Juan Travolta.&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;For the month of October, John Travolta is officially Mexican.&lt;/p&gt;",
+			"published": "2008-10-12T04:12:22Z",
+			"author": "nobody@flickr.com (a.meadowlark)",
+			"author_id": "82865775@N00",
+			"tags": "blue cat turquoise tuxedo"
+	   },
+	   {
+			"title": "IMGP3713",
+			"link": "http://www.flickr.com/photos/takewrning/2933648786/",
+			"media": {"m":"http://farm4.static.flickr.com/3190/2933648786_558e017a82_m.jpg"},
+			"date_taken": "2008-10-11T23:59:23-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/takewrning/&quot;&gt;Petie_in_Cbus&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/takewrning/2933648786/&quot; title=&quot;IMGP3713&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3190/2933648786_558e017a82_m.jpg&quot; width=&quot;240&quot; height=&quot;160&quot; alt=&quot;IMGP3713&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:12:39Z",
+			"author": "nobody@flickr.com (Petie_in_Cbus)",
+			"author_id": "56257767@N00",
+			"tags": "cats cat creatures"
+	   },
+	   {
+			"title": "",
+			"link": "http://www.flickr.com/photos/ameadowlark/2932791441/",
+			"media": {"m":"http://farm4.static.flickr.com/3174/2932791441_eb25349c9a_m.jpg"},
+			"date_taken": "2008-10-11T21:38:10-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/ameadowlark/&quot;&gt;a.meadowlark&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/ameadowlark/2932791441/&quot; title=&quot;&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3174/2932791441_eb25349c9a_m.jpg&quot; width=&quot;240&quot; height=&quot;161&quot; alt=&quot;&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:11:51Z",
+			"author": "nobody@flickr.com (a.meadowlark)",
+			"author_id": "82865775@N00",
+			"tags": "blue cat table"
+	   },
+	   {
+			"title": "Abbey",
+			"link": "http://www.flickr.com/photos/30201052@N07/2933641254/",
+			"media": {"m":"http://farm4.static.flickr.com/3030/2933641254_14f2bf8deb_m.jpg"},
+			"date_taken": "2008-09-23T20:09:13-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/30201052@N07/&quot;&gt;lowellsimonsen&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/30201052@N07/2933641254/&quot; title=&quot;Abbey&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3030/2933641254_14f2bf8deb_m.jpg&quot; width=&quot;240&quot; height=&quot;159&quot; alt=&quot;Abbey&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;very intent&lt;/p&gt;",
+			"published": "2008-10-12T04:08:53Z",
+			"author": "nobody@flickr.com (lowellsimonsen)",
+			"author_id": "30201052@N07",
+			"tags": "red cat chair"
+	   },
+	   {
+			"title": "pooter in the cat-art gallery",
+			"link": "http://www.flickr.com/photos/22522979@N02/2933641318/",
+			"media": {"m":"http://farm4.static.flickr.com/3059/2933641318_68e462ec4b_m.jpg"},
+			"date_taken": "2008-10-11T21:08:55-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/22522979@N02/&quot;&gt;great2c&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/22522979@N02/2933641318/&quot; title=&quot;pooter in the cat-art gallery&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3059/2933641318_68e462ec4b_m.jpg&quot; width=&quot;172&quot; height=&quot;240&quot; alt=&quot;pooter in the cat-art gallery&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:08:55Z",
+			"author": "nobody@flickr.com (great2c)",
+			"author_id": "22522979@N02",
+			"tags": "cats cat kitten kittens"
+	   },
+	   {
+			"title": "calendar2009preview",
+			"link": "http://www.flickr.com/photos/26792147@N04/2932778861/",
+			"media": {"m":"http://farm4.static.flickr.com/3269/2932778861_08105763b3_m.jpg"},
+			"date_taken": "2008-10-11T21:05:52-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/26792147@N04/&quot;&gt;Marks Mysteries&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/26792147@N04/2932778861/&quot; title=&quot;calendar2009preview&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3269/2932778861_08105763b3_m.jpg&quot; width=&quot;192&quot; height=&quot;240&quot; alt=&quot;calendar2009preview&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;Preview of My Cats 2009 Calendar. Available at LULU. &lt;a href=&quot;http://www.lulu.com/content/4452330&quot;&gt;www.lulu.com/content/4452330&lt;/a&gt;&lt;/p&gt;",
+			"published": "2008-10-12T04:05:52Z",
+			"author": "nobody@flickr.com (Marks Mysteries)",
+			"author_id": "26792147@N04",
+			"tags": "cat kitten calendarcalendarcatkitten"
+	   },
+	   {
+			"title": "pooter... and her listening cd's",
+			"link": "http://www.flickr.com/photos/22522979@N02/2932770897/",
+			"media": {"m":"http://farm4.static.flickr.com/3064/2932770897_74bbb3301a_m.jpg"},
+			"date_taken": "2008-10-11T21:02:00-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/22522979@N02/&quot;&gt;great2c&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/22522979@N02/2932770897/&quot; title=&quot;pooter... and her listening cd's&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3064/2932770897_74bbb3301a_m.jpg&quot; width=&quot;240&quot; height=&quot;205&quot; alt=&quot;pooter... and her listening cd's&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:02:00Z",
+			"author": "nobody@flickr.com (great2c)",
+			"author_id": "22522979@N02",
+			"tags": "cats cat kitten kittens"
+	   },
+	   {
+			"title": "Abbey",
+			"link": "http://www.flickr.com/photos/30201052@N07/2933641250/",
+			"media": {"m":"http://farm4.static.flickr.com/3003/2933641250_4b8e983d5e_m.jpg"},
+			"date_taken": "2008-09-17T17:32:56-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/30201052@N07/&quot;&gt;lowellsimonsen&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/30201052@N07/2933641250/&quot; title=&quot;Abbey&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3003/2933641250_4b8e983d5e_m.jpg&quot; width=&quot;240&quot; height=&quot;159&quot; alt=&quot;Abbey&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;chasing the hose&lt;/p&gt;",
+			"published": "2008-10-12T04:08:53Z",
+			"author": "nobody@flickr.com (lowellsimonsen)",
+			"author_id": "30201052@N07",
+			"tags": "cat hose"
+	   },
+	   {
+			"title": "Cat Tree",
+			"link": "http://www.flickr.com/photos/9107582@N02/2933624616/",
+			"media": {"m":"http://farm4.static.flickr.com/3213/2933624616_7bf02d88cb_m.jpg"},
+			"date_taken": "2008-09-03T01:22:46-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/9107582@N02/&quot;&gt;prissyqods&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/9107582@N02/2933624616/&quot; title=&quot;Cat Tree&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3213/2933624616_7bf02d88cb_m.jpg&quot; width=&quot;127&quot; height=&quot;240&quot; alt=&quot;Cat Tree&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;All 3 cats coming home from their yearly vet appointment.&lt;br /&gt; Tamara on top, Nalu in the middle &amp;amp; Samson on the bottom.&lt;/p&gt;",
+			"published": "2008-10-12T04:00:45Z",
+			"author": "nobody@flickr.com (prissyqods)",
+			"author_id": "9107582@N02",
+			"tags": "cats tree cat vet carrier stacked"
+	   },
+	   {
+			"title": "asha ready for halloween IT'S ALL ABOUT THE EARS AND EYES ...",
+			"link": "http://www.flickr.com/photos/22522979@N02/2933634752/",
+			"media": {"m":"http://farm4.static.flickr.com/3151/2933634752_e88878541d_m.jpg"},
+			"date_taken": "2008-10-11T21:05:52-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/22522979@N02/&quot;&gt;great2c&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/22522979@N02/2933634752/&quot; title=&quot;asha ready for halloween IT'S ALL ABOUT THE EARS AND EYES ...&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3151/2933634752_e88878541d_m.jpg&quot; width=&quot;240&quot; height=&quot;173&quot; alt=&quot;asha ready for halloween IT'S ALL ABOUT THE EARS AND EYES ...&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T04:05:52Z",
+			"author": "nobody@flickr.com (great2c)",
+			"author_id": "22522979@N02",
+			"tags": "cats cat kitten kittens"
+	   },
+	   {
+			"title": "The Claw and the Blue feather.",
+			"link": "http://www.flickr.com/photos/photosbylamont/2932772595/",
+			"media": {"m":"http://farm4.static.flickr.com/3142/2932772595_f1b199eddc_m.jpg"},
+			"date_taken": "2008-10-10T22:56:52-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/photosbylamont/&quot;&gt;Photos by Lamont&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/photosbylamont/2932772595/&quot; title=&quot;The Claw and the Blue feather.&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3142/2932772595_f1b199eddc_m.jpg&quot; width=&quot;240&quot; height=&quot;179&quot; alt=&quot;The Claw and the Blue feather.&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;The claws come out just before striking.&lt;/p&gt;",
+			"published": "2008-10-12T04:02:52Z",
+			"author": "nobody@flickr.com (Photos by Lamont)",
+			"author_id": "63155741@N00",
+			"tags": "animal cat feline toes dof sb600 claw hunter speedlight himalayan nikond200 bluefeather strobist photosbylamont"
+	   },
+	   {
+			"title": "Ellie May",
+			"link": "http://www.flickr.com/photos/9107582@N02/2933603648/",
+			"media": {"m":"http://farm4.static.flickr.com/3154/2933603648_4437348733_m.jpg"},
+			"date_taken": "2008-10-11T16:19:48-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/9107582@N02/&quot;&gt;prissyqods&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/9107582@N02/2933603648/&quot; title=&quot;Ellie May&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3154/2933603648_4437348733_m.jpg&quot; width=&quot;240&quot; height=&quot;195&quot; alt=&quot;Ellie May&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;Tongue in action&lt;/p&gt;",
+			"published": "2008-10-12T03:50:54Z",
+			"author": "nobody@flickr.com (prissyqods)",
+			"author_id": "9107582@N02",
+			"tags": "tongue cat lick calico"
+	   },
+	   {
+			"title": "Commander Adama",
+			"link": "http://www.flickr.com/photos/chibikitsune/2933603538/",
+			"media": {"m":"http://farm4.static.flickr.com/3031/2933603538_ec9f2555a6_m.jpg"},
+			"date_taken": "2008-10-11T18:04:18-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/chibikitsune/&quot;&gt;chibikitsune&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/chibikitsune/2933603538/&quot; title=&quot;Commander Adama&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3031/2933603538_ec9f2555a6_m.jpg&quot; width=&quot;240&quot; height=&quot;180&quot; alt=&quot;Commander Adama&quot; /&gt;&lt;/a&gt;&lt;/p&gt; ",
+			"published": "2008-10-12T03:50:51Z",
+			"author": "nobody@flickr.com (chibikitsune)",
+			"author_id": "24898771@N00",
+			"tags": "cat himalayan"
+	   },
+	   {
+			"title": "Tamara",
+			"link": "http://www.flickr.com/photos/9107582@N02/2932752655/",
+			"media": {"m":"http://farm4.static.flickr.com/3183/2932752655_cc7a797955_m.jpg"},
+			"date_taken": "2008-09-30T03:11:04-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/9107582@N02/&quot;&gt;prissyqods&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/9107582@N02/2932752655/&quot; title=&quot;Tamara&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3183/2932752655_cc7a797955_m.jpg&quot; width=&quot;240&quot; height=&quot;205&quot; alt=&quot;Tamara&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;All stretched out while taking a snooze in the sun.&lt;/p&gt;",
+			"published": "2008-10-12T03:53:29Z",
+			"author": "nobody@flickr.com (prissyqods)",
+			"author_id": "9107582@N02",
+			"tags": "sun black cat sleep nail stretch claw"
+	   },
+	   {
+			"title": "Cat Tails",
+			"link": "http://www.flickr.com/photos/9107582@N02/2933609654/",
+			"media": {"m":"http://farm4.static.flickr.com/3230/2933609654_cff1f44750_m.jpg"},
+			"date_taken": "2008-09-30T03:11:41-08:00",
+			"description": "&lt;p&gt;&lt;a href=&quot;http://www.flickr.com/people/9107582@N02/&quot;&gt;prissyqods&lt;/a&gt; posted a photo:&lt;/p&gt; &lt;p&gt;&lt;a href=&quot;http://www.flickr.com/photos/9107582@N02/2933609654/&quot; title=&quot;Cat Tails&quot;&gt;&lt;img src=&quot;http://farm4.static.flickr.com/3230/2933609654_cff1f44750_m.jpg&quot; width=&quot;240&quot; height=&quot;238&quot; alt=&quot;Cat Tails&quot; /&gt;&lt;/a&gt;&lt;/p&gt; &lt;p&gt;Samson, Tamara &amp;amp; Nalu ... all tails, all the time!&lt;/p&gt;",
+			"published": "2008-10-12T03:53:40Z",
+			"author": "nobody@flickr.com (prissyqods)",
+			"author_id": "9107582@N02",
+			"tags": "cats cat tail names tails"
+	   }
+        ]
+}
